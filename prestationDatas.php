4a8b7023d8641f19efe3b6a293a8e31277e2ed02
AudeP <aude.plana@gmail.com>
2020-10-09 09:05:44 +0200

    creation array prestation

--- prestationDatas.php
+++ prestationDatas.php
@@ -1,1 +1,34 @@
 <?php
+
+$prestationDatas = [
+    $kitchens =
+        ['src1' => 'paragraph1',
+        'src2' => 'paragraph2',
+        'src3' => 'paragraph3',
+        'src4' => 'paragraph4',
+        'src5' => 'paragraph5',
+    ],
+    $livingrooms =
+        ['src01' => 'paragraph01',
+        'src02' => 'paragraph02',
+        'src03' => 'paragraph03',
+        'src04' => 'paragraph04',
+        'src05' => 'paragraph05',
+    ],
+];
+
+echo $prestationDatas[$kitchens] ['src2'];
+
+
+/*var_dump ($kitchens);*/
+
+
+/*$kitchens = [
+    'src1' => 'paragraf1',
+    'src2' => 'paragraf2',
+    'src3' => 'paragraf3',
+];
+
+echo $kitchens ['src2'];*/
+
+
